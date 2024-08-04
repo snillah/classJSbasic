db63a8e21358316d6c79a103df45cedb2631af54
saravanan <sarvanillah@gmail.com>
2024-08-04 12:59:22 +0530

    4-8-2024 class added

---
+++ 4-8-2024/script.js
@@ -0,0 +1,65 @@
+// console.log(this);
+
+// console.log("working")
+
+// alert("are you approving")
+// console.log("approved");
+
+// // prompt()
+// let data = prompt("Enter the Name","user");
+// console.log(data);
+
+// // confirm()
+
+// let flag = confirm("can you willing to come to the class")
+
+// console.log("flag value",flag);
+
+// if(flag === true){
+//     console.log("Thanks for your consern");
+// }else{
+//     console.log("take care thanks");
+    
+// }
+
+// variable => memory to store the values
+
+// dataType => string, number,boolean, Object, array
+// values =>  user1,26,true,{language}
+
+let userName = "user1";
+let userAge = 28;
+let isStudent = true;
+const userInfo = {language:["tamil","English"],married:false}
+const userHobbies =["playing football","listing music","story writting"]
+
+console.log("name :",userName,typeof userName);
+console.log("age :",userAge, typeof userAge);
+console.log("Is Student :",isStudent, typeof isStudent);
+console.log("details :",userInfo, typeof userInfo);
+console.log("Hobiles :",userHobbies,typeof userHobbies);
+
+//arithmetic operations
+
+console.log("Arithmetic Operation");
+
+let a =5;
+let b =7;
+
+console.log(`Add ${a} + ${b} =`,a+b);
+console.log(`Sub ${a} - ${b} =`,a-b);
+console.log(`mutiple ${a} * ${b} =`,a*b);
+console.log(`division ${a} / ${b} =`,a/b);
+console.log(`modules ${a} % ${b} =`,a%b);
+
+console.log("logical Operator");
+
+console.log(`And ${a} === ${b}`, a===b);
+console.log(`And ${a} !== ${b}`, a!==b);    
+console.log(`And ${a} && ${b}`, a==5&&b==7);
+console.log(`And ${a} || ${b}`, a==10 || b==7 );
+
+
+
+
+
