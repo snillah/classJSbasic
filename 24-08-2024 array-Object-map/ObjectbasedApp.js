a646135fa560a95ed086e60c03d759e7e94e13b9
saravanan <sarvanillah@gmail.com>
2024-08-24 22:39:41 +0530

    update the array choosing object based or hash map view

--- 24-08-2024 array-Object-map/ObjectbasedApp.js
+++ 24-08-2024 array-Object-map/ObjectbasedApp.js
@@ -2,7 +2,7 @@ var arr=[2,3,4,5,5,5,7,7,7,8,9,3,6]
 
 var temp={}
 var min_occ = 1;
-var max_value;
+var max_value =[];
 
 for(let i =0;i<arr.length;i++){
     let ele = arr[i]
@@ -14,8 +14,9 @@ for(let i =0;i<arr.length;i++){
     }
     if(temp[ele]>min_occ){
         min_occ = temp[ele]
-        max_value = ele
+        max_value=[ele] 
     }
 
 }
 console.log("Object value",temp);
+console.log("Higher value",max_value);
